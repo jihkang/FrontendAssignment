6940c9232e15516dd4667be833105adaf4d09814
jihkang <106561135+jihkang@users.noreply.github.com>
2024-06-24 14:02:43 +0900

    Feat: columns added
    칼럼간 카드 이동구현

---
+++ data.js
@@ -0,0 +1,4 @@
+export const init_data = {
+  tasks: [],
+  columns: [0, 1, 2, 3],
+};
--- index.js
+++ index.js
@@ -7,16 +7,21 @@ function App() {
     Array.from({ length: count }, (v, k) => k).map((k) => ({
       id: `item-${k}`,
       content: `item ${k}`,
+      selected: false,
     }));
 
-  const [items, setItems] = useState(getItems(10));
-  const [otherColumns, setOtherColumns] = useState([]);
+  const [items, setItems] = useState({
+    column: getItems(10),
+    column2: [],
+    column3: [],
+    column4: [],
+  });
 
-  const reorder = (list, startIndex, endIndex) => {
-    const result = Array.from(list);
-    const [removed] = result.splice(startIndex, 1);
-    result.splice(endIndex, 0, removed);
-    return result;
+  const reorder = (source, destination) => {
+    const _items = JSON.parse(JSON.stringify(items));
+    const [removed] = _items[source.droppableId].splice(source.index, 1);
+    _items[destination.droppableId].splice(destination.index, 0, removed);
+    return _items;
   };
 
   const onDragEnd = useCallback(
@@ -24,64 +29,64 @@ function App() {
       if (!result.destination) {
         return;
       }
-
-      const newItems = reorder(
-        items,
-        result.source.index,
-        result.destination.index
-      );
-
-      setItems(newItems);
+      const { destination, source } = result;
+      setItems(reorder(source, destination));
     },
     [items]
   );
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="droppable">
-        {(provided, snapshot) => (
-          <div
-            {...provided.droppableProps}
-            ref={provided.innerRef}
-            style={getListStyle(snapshot.isDraggingOver)}
-          >
-            {items.map((item, index) => (
-              <Draggable key={item.id} draggableId={item.id} index={index}>
-                {(provided, snapshot) => (
-                  <div
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    style={getItemStyle(
-                      snapshot.isDragging,
-                      provided.draggableProps.style
+      <div style={{ display: "grid", gridTemplateColumns: "repeat(4, 1fr)" }}>
+        {Object.keys(items).map((key) => (
+          <Droppable droppableId={key} key={`droppabled_column_${key}`}>
+            {(provided, snapshot) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                style={getListStyle(snapshot.isDraggingOver)}
+              >
+                <span>{key}: column</span>
+                {items[key].map((item, index) => (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps.style
+                        )}
+                      >
+                        {item.content}
+                      </div>
                     )}
-                  >
-                    {item.content}
-                  </div>
-                )}
-              </Draggable>
-            ))}
-
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
+                  </Draggable>
+                ))}
+              </div>
+            )}
+          </Droppable>
+        ))}
+      </div>
     </DragDropContext>
   );
 }
 
 const GRID = 8;
 
-const getItemStyle = (isDragging, draggableStyle) => ({
+const getItemStyle = (isDragging, draggableStyle, isSelected) => ({
   userSelect: "none",
   padding: GRID * 2,
   margin: `0 0 ${GRID}px 0`,
-  background: isDragging ? "lightgreen" : "grey",
+  background: isDragging ? "lightgreen" : isSelected ? "green" : "grey",
   ...draggableStyle,
 });
 
 const getListStyle = (isDraggingOver) => ({
+  display: "flex",
+  flexDirection: "column",
+  gap: "8px",
   background: isDraggingOver ? "lightblue" : "lightgrey",
   padding: GRID,
   width: 250,
